f036a4ba2cb5ebe66c674bee8eb2e05114617da5
AHopkins17 <ahopkins@students.mchenry.edu>
2024-08-26 18:32:29 -0500

    Add files via upload

---
+++ # Knock-Knock Joke Program.py
@@ -0,0 +1,9 @@
+# Knock-Knock Joke Program
+print("Knock, knock.")
+print("Who's there?")
+print("Yah.")
+print("Yah who?")
+print("Sorry, I prefer Google.")
+print("\n")
+# As one line of code using escape characters
+print("Knock, knock.\nWho's there?\nYah.\nYah who?\nSorry, I prefer Google.")
