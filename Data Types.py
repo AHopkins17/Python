58b4443c55fd48018a854f19f0a2956fb615b4f7
AHopkins17 <ahopkins@students.mchenry.edu>
2024-08-26 19:37:02 -0500

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,21 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 15
+print("Literal:", x)
+
+# Integer
+y = -15
+print("Integer:", y)
+
+# Float
+z = 12.6
+print("Float:", z)
+
+# String
+name = "Annette"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
