7d3beefc0995927c1a2f5405e60006c4931b2843
AHopkins17 <ahopkins@students.mchenry.edu>
2024-08-26 20:35:06 -0500

    Add files via upload

---
+++ Mathematical Operations.py
@@ -0,0 +1,9 @@
+# Area of Rectangle
+# l is the length and w is the width of the rectangle.
+# a is the area
+l = 5
+w = 10
+a = l * w
+print("Area of Rectangle: ", a) 
+print("\n")
+print("The area of the rectangle with a length of 5 and a width of 10 is 50")
