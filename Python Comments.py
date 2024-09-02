ac4051c2f207cf5b247e819a17b5b21340ffc82e
AHopkins17 <ahopkins@students.mchenry.edu>
2024-09-02 03:06:34 -0500

    Add files via upload

---
+++ Python Comments.py
@@ -0,0 +1,11 @@
+# Define variables for gallons values
+tank_size = 15
+
+# Define variables for distance values
+distance = 600
+
+# Calculate mpg for each distance and tank_size
+mpg = distance / tank_size
+
+# Output the results
+print("Miles per gallon:", mpg)
