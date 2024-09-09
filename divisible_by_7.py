3e29da6ea21ff9c5653cf43b7a4fc2564e4a3cf3
AHopkins17 <ahopkins@students.mchenry.edu>
2024-09-09 07:17:18 -0500

    Add files via upload

---
+++ divisible_by_7.py
@@ -0,0 +1,11 @@
+# Iterate through the numbers from 1 to 300
+
+for numbers in range(1, 300):
+    
+    # Check numbers that are divisible by 7
+
+    if numbers % 7 ==0:
+
+        # Print the numbers that are divisible by 7
+
+        print(numbers)
