64713848dcd64a0943e7b55a8b82b285ee539566
AHopkins17 <ahopkins@students.mchenry.edu>
2024-09-19 03:39:59 -0500

    Add files via upload

---
+++ area_program.py
@@ -0,0 +1,17 @@
+# Create a Program to Calculate Areas
+
+# Area of Square
+def square(side):
+    area = side * side
+    print("The area of the square is " + str(area) + " square units.")
+
+# Area of Circle
+def circle(radius):
+    pi = 3.14
+    area = pi * radius * radius
+    print("The area of the circle is " + str(area) + " square units.")
+
+# Test functions
+square(8)
+circle(7)
+# Change these numbers to test different values
