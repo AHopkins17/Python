1abd1f5afef820c5f399da0d8a65f5350154a0aa
AHopkins17 <ahopkins@students.mchenry.edu>
2024-11-04 02:01:21 -0600

    Add files via upload

---
+++ book_titles_collector.py
@@ -0,0 +1,23 @@
+def main():
+    # Setting up list to collect book titles
+    book_titles = []
+
+    # Collecting book titles using a while loop
+    while len(book_titles) < 10:
+        
+        title = input("Enter a book title: ")
+        # Capitalization
+        book_titles.append(title.upper())
+
+    # Sorted List
+    sorted_titles = sorted(book_titles)
+
+    # Titles List 
+    print("\nHere are you book titles in alphabetical order:")
+
+    for title in sorted_titles:
+        print(title)
+
+# Test Program
+if __name__== "__main__":
+    main()
